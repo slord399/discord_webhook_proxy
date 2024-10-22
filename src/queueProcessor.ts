0e6fa2b624063b4ba2efdf452e741a7032e63829
Tony Lewis Hiroaki URAHAMA <50810875+slord399@users.noreply.github.com>
2024-10-22 09:30:12 +0100

    Revert

--- src/queueProcessor.ts
+++ src/queueProcessor.ts
@@ -41,7 +41,7 @@ async function run() {
     }
 
     log('Consuming.');
-    await rabbitMq.prefetch(100);
+    await rabbitMq.prefetch(10);
     await rabbitMq.consume(
         config.queue.queue,
         async msg => {
