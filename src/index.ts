9a7aacc10179286493d88e3ff57eedd1a2c17b18
Tony Lewis Hiroaki URAHAMA <50810875+slord399@users.noreply.github.com>
2025-07-25 15:04:00 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -700,7 +700,7 @@ const rateLimit = require('express-rate-limit');
 
 const deleteRateLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
+    max: 1000, // limit each IP to 1000 requests per windowMs
     message: {
         proxy: true,
         error: 'Too many requests, please try again later.'
